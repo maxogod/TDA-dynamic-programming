39cb1ecb6888e3346c6266d900c588d0c196806c
maxogod <maxi18wars@gmail.com>
2024-02-03 18:54:26 -0300

    algoritmo funcionando - test pasando

--- algoritmo.py
+++ algoritmo.py
@@ -1,9 +1,22 @@
 
 #!/usr/bin/env python3
 
-# OPT(i) = max(  OPT(i-2) + min(s1, ei), M[i-1][j] + min(sj+1, ei) for j in range(i-1)  )
+# OPT(i) = max(  OPT(i-2) + min(s1, ei), M[i-1][j-1] + min(sj, ei) for j in range(1, i)  )
 def obtener_ganancia_maxima(esfuerzos, energias) -> int:
-    return 0
+    memo = [[0] * len(esfuerzos) for _ in range(len(esfuerzos))]  # matriz nxn
+
+    memo[0][0] = min(esfuerzos[0], energias[0])  # caso base
+    memo[1][0] = min(esfuerzos[1], energias[0])
+
+    for i in range(1, len(esfuerzos)):
+
+        if i != 1:
+            memo[i][0] = max(memo[i-2]) + min(esfuerzos[i], energias[0])
+
+        for j in range(1, i+1):
+            memo[i][j] = memo[i-1][j-1] + min(esfuerzos[i], energias[j])
+
+    return max(memo[-1])
 
 
 def recontruir_camino(memo, esfuerzos, energias) -> list[str]:
