353a3bbdb90856e39bb47314babff9a05ee5db4c
maxogod <maxi18wars@gmail.com>
2024-02-03 18:02:09 -0300

    recurrencia

--- algoritmo.py
+++ algoritmo.py
@@ -1,6 +1,7 @@
 
 #!/usr/bin/env python3
 
+# OPT(i) = max(  OPT(i-2) + min(s1, ei), M[i-1][j] + min(sj+1, ei) for j in range(i-1)  )
 def obtener_ganancia_maxima(esfuerzos, energias) -> int:
     return 0
 
